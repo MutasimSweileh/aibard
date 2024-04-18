40cf2617f7500eb408d2f4d85d682944f2b51082
Mutasim Sweileh <mutasimsweileh@gmail.com>
2024-04-18 04:20:26 +0200

    modified:   index.php

--- index.php
+++ index.php
@@ -36,8 +36,6 @@ function _in_str($str, $arr = null, $lower = true, &$word = "")
 function _file_get_contents($url, $headers = [], $data = null, $timeout = 30, &$error = null)
 {
 
-    if (_in_str($url, ["app.restoviebelle", "everysimply"]))
-        $headers[] = 'Authorization: Basic bW9odGFzbTptb2h0YXNtMTBRQEA=';
     $agent = array_filter($headers, fn ($proxy) => (_in_str($proxy, "User-Agent")));
     if (!$agent) {
         $headers[] = "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36";
@@ -46,9 +44,6 @@ function _file_get_contents($url, $headers = [], $data = null, $timeout = 30, &$
     $f = array_filter($headers, fn ($proxy) => (_in_str($proxy, "form-urlencoded") && _in_str($proxy, "Content-Type")));
     $data = ($data && $j ? json_encode($data) : $data);
     $data = ($data && $f ? http_build_query($data) : $data);
-    #print_r($data);
-    // echo $url;
-    // print_r($headers);
     $curl = curl_init();
     curl_setopt_array($curl, array(
         CURLOPT_URL => $url,
@@ -75,41 +70,38 @@ function _file_get_contents($url, $headers = [], $data = null, $timeout = 30, &$
 
 function bard($d = [])
 {
-    $keys = [
-        "AIzaSyDbEKAYJICTvG45GQyYHh97LbJmAMJ4dEk",
-        "AIzaSyDn_j28y4bfXyulcxpIIUYjcZi7d3Bidz8",
-    ];
     if (!$d["prompt"]) {
         $d["prompt"] = isset($d["content"]) ? $d["content"] : null;
     }
+
+    $safetySettings = [];
+    $safetySettings[] = ["category" => "HARM_CATEGORY_HARASSMENT", "threshold" => "BLOCK_NONE"];
+    $safetySettings[] = ["category" => "HARM_CATEGORY_HATE_SPEECH", "threshold" => "BLOCK_NONE"];
+    $safetySettings[] = ["category" => "HARM_CATEGORY_SEXUALLY_EXPLICIT", "threshold" => "BLOCK_NONE"];
+    $safetySettings[] = ["category" => "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold" => "BLOCK_NONE"];
     $d += [
-        "temperature" => 0.7,
+        "maxOutputTokens" => 2048,
+        "temperature" => 0.9,
+        "topK" => 1,
         "topP" => 1,
-        "key" => null
+        "key" => null,
+        "safetySettings" =>  $safetySettings
     ];
-    $j = json_decode('{ 
-      "contents": [
-            {
-              "parts": []
-            }
-       ],
-      "generationConfig": {
-        "temperature": 0.9,
-        "topK": 1,
-        "topP": 1,
-        "maxOutputTokens": 2048,
-        "stopSequences": []
-      }}', true);
-    $j["contents"][0]["parts"][] = ["text" => $d["prompt"]];
-    $j["safetySettings"][] = ["category" => "HARM_CATEGORY_HARASSMENT", "threshold" => "BLOCK_NONE"];
-    $j["safetySettings"][] = ["category" => "HARM_CATEGORY_HATE_SPEECH", "threshold" => "BLOCK_NONE"];
-    $j["safetySettings"][] = ["category" => "HARM_CATEGORY_SEXUALLY_EXPLICIT", "threshold" => "BLOCK_NONE"];
-    $j["safetySettings"][] = ["category" => "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold" => "BLOCK_NONE"];
-    $j["generationConfig"]["temperature"] = $d["temperature"];
-    $j["generationConfig"]["topP"] = $d["topP"];
-    //return $j;
-    $index = 0;
-    $key = $keys[$index];
+    extract($d);
+    $j = [];
+    $j["contents"] = [
+        "parts" => [
+            ["text" => $prompt]
+        ]
+    ];
+    $j["safetySettings"] = $safetySettings;
+    $j["generationConfig"] = [
+        "temperature" => $temperature,
+        "topK" => $topK,
+        "topP" => $topP,
+        "maxOutputTokens" => $maxOutputTokens,
+        "stopSequences" => []
+    ];
     $jsonData = _file_get_contents(
         'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.0-pro:generateContent?key=' . $key,
         ['Content-Type: application/json'],
@@ -117,30 +109,23 @@ function bard($d = [])
         (3 * 60)
     );
     $json = json_decode($jsonData, true);
-    unset($d["content"]);
-    unset($d["prompt"]);
-    $return = ["success" => true];
-    #return $json;
-    if (!isset($json["candidates"]) || !$json || !isset($json["candidates"][0]) || !isset($json["candidates"][0]["content"])) {
-        $finish_reason = isset($json["candidates"][0]["finishReason"]) ? $json["candidates"][0]["finishReason"] : null;
-        $error = ($json && isset($json["error"]) ? $json["error"] : ($json ? $json : $jsonData));
-        $error = $finish_reason ? $finish_reason : $error;
-        $return = ["success" => false, "error" => $error];
-        $return["error"] = (isset($return["error"]["message"]) ? $return["error"]["message"] : $return["error"]);
-        if (is_array($return["error"])) {
-            $return["error"] = json_encode($return["error"]);
-        }
-        return $return;
-    } else if (isset($json["candidates"])) {
-        $return["content"] = $json["candidates"][0]["content"]["parts"][0]["text"];
-        //$return["safetyRatings"] = $json["candidates"][0]["safetyRatings"];
-        $return["finish_reason"] = $json["candidates"][0]["finishReason"];
-        $return["account"] = ["index" => $index, "key" => $key];
-        unset($json["candidates"]);
-        unset($json["promptFeedback"]);
+    return $json;
+}
+
+function send_request($d = [])
+{
+    $d += [
+        "url" => null,
+        "headers" => [],
+        "data" => [],
+        "timeout" => 60
+    ];
+    extract($d);
+    $response = _file_get_contents($url, $headers, $data, $timeout, $err);
+    if ($response) {
+        return ["success" => true, "data" => $response];
     }
-    $return = array_merge($return, $json);
-    return $return;
+    return ["success" => false, "error" => $err];
 }
 $data = json_decode(file_get_contents('php://input'), true);
 if ($data && isset($data["data"])) {
@@ -159,11 +144,16 @@ $_POST = array_map(function ($v) {
     }
     return $v;
 }, $_POST);
-$get = isv("get");
 
+
+$get = isv("get");
 switch ($get) {
     case 'bard':
         $response = bard($_POST);
+        echo json_encode($response);
+        break;
+    case 'request':
+        $response = send_request($_POST);
         echo json_encode($response);
         break;
     default:
